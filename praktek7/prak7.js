1a0f22317bc67b7b6c98a5505aaef2729909eccd
GlennMoreno <lolglenn26@gmail.com>
2024-01-22 14:00:13 +0800

    first commit

---
+++ praktek7/prak7.js
@@ -0,0 +1,50 @@
+// let silaPancasila = 1;
+
+// switch (silaPancasila) {
+//     case1:
+//      document.write("Ketuhann Yang Maha Esa");
+//      break;
+//      case2:
+//      document.write("Kemanusiaan Yang Adil dan Beradab");
+//      break;
+//      case3:
+//      document.write("Persatuan Indonesia");
+//      break;
+//      case4:
+//      document.write("Kerakyatan Yang Dipimpin Oleh Hikmat Kebijaksanaan, Dalam Permusyawaratan Perwakilan");
+//      break;
+//      case5:
+//      document.write("Keadilan Sosial Bagi Seluruh Rakyat Indonesia");
+//      break;
+//     default:
+//         document.write("Pancasila");
+    
+
+// }
+
+let platNomor = 'DA';
+
+switch (platNomor) {
+    case 'KU':
+        document.write("Plat Nomor Polisi Daerah Kalimantan Utara");
+    break;
+
+    case 'KT':
+        document.write("Plat Nomor Polisi Daerah Kalimantan Timur");
+    break;
+
+    case 'DA':
+        document.write("Plat Nomor Polisi Daerah Kalimantan Selatan");
+    break;
+
+    case 'KB':
+        document.write("Plat Nomor Polisi Daerah Kalimantan Barat");
+    break;
+
+    case 'KH':
+        document.write("Plat Nomor Polisi Daerah Kalimantan Tengah");
+    break;
+
+    default :
+        document.write("Anda bukan orang kalimantan rek")
+}
